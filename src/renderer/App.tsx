0172592ffcbf470ffabe0e84408566b91e032b2a
Nadav Inbar <nadav.inbar@enwize-tw.com>
2023-05-10 16:28:07 +0300

    deleted old electron stuff

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,44 +5,6 @@ import Layout from './components/Layout/Layout';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Champion from './pages/Champion/Champion';
 
-// function Hello() {
-//   return (
-//     <div>
-//       <div className="Hello">
-//         <img width="200" alt="icon" src={icon} />
-//       </div>
-//       <h1>electron-react-boilerplate</h1>
-//       <div className="Hello">
-//         <a
-//           href="https://electron-react-boilerplate.js.org/"
-//           target="_blank"
-//           rel="noreferrer"
-//         >
-//           <button type="button">
-//             <span role="img" aria-label="books">
-//               📚
-//             </span>
-//             Read our docs
-//           </button>
-//         </a>
-//         <a
-//           href="https://github.com/sponsors/electron-react-boilerplate"
-//           target="_blank"
-//           rel="noreferrer"
-//         >
-//           <button type="button">
-//             <span role="img" aria-label="folded hands">
-//               🙏
-//             </span>
-//             Donate
-//           </button>
-//         </a>
-//       </div>
-//       <Layout />
-//     </div>
-//   );
-// }
-
 export default function App() {
   return (
     <Router>
